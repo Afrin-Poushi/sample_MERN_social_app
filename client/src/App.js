1f98d7e7992e25345ecc3f360dad2ef40e01f9fc
Poushi <afrinsultana@iut-dhaka.edu>
2023-03-17 12:48:04 +0600

    created pages and set routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,27 @@
-import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  NavLink,
+  Navigate,
+  BrowserRouter,
+} from "react-router-dom";
 
+import HomePage from "./pages/homePage";
+import LoginPage from "./pages/loginPage";
+import ProfilePage from "./pages/profilePage";
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />}></Route>
+          <Route path="/home" element={<HomePage />}></Route>
+          <Route path="/profile:userId" element={<ProfilePage />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/pages/profilePage/index.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+
+const ProfilePage = () => {
+    return  <div>ProfilePage</div>;
+};
+
+export default ProfilePage
